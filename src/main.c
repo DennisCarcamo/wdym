c5e2a8835dd4b953cf0c40b35fd7a1e8ebc98bcf
cano <canwoahzo@gmail.com>
2019-08-15 00:15:48 -0600

    leer comandos

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
+#include <sys/wait.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Comandos de prueba:
+// ls, ping, cat, help, top
+
+char **split(char *);
+
 int main() {
   int child;
-  size_t bufsize = 32;
+  size_t bufsize = 128;
   size_t read;
   char *buffer = (char *) malloc(bufsize);
 
@@ -16,7 +23,49 @@ int main() {
     if (strcmp(buffer, "exit") == 0) {
       break;
     }
+
+    char **vector = split(buffer);
+
+    child = fork();
+    if (child == 0) {
+      execvp(vector[0], vector);
+
+      // si llega a este punto hubo un error
+      printf("error: comando \"%s\" no se pudo ejecutar.\n", buffer);
+      exit(1);
+    } else {
+      wait(&child);
+      free(vector);
+    }
   }
 
+  free(buffer);
   return 0;
+}
+
+char **split(char *input) {
+  // tomar una cadena y hacerle un split por cada espacio:
+  // "ls -l" => ["ls", "-l"]
+
+  int allocated = 4;
+  char **res = (char **) malloc(sizeof(*res) * allocated);
+
+  int size = 0;
+  char *token = strtok(input, " \t");
+  while (token != NULL) {
+    if (size >= allocated) {
+      allocated *= 2;
+      res = realloc(res, sizeof(*res) * allocated);
+    }
+
+    res[size] = token;
+    size += 1;
+    token = strtok(NULL, " \t");
+  }
+
+  if (allocated > size) {
+    res[size] = 0;
+  }
+
+  return res;
 }
